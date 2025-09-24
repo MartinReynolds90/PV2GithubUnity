1cb5fe365e9daf17dd4ae9570c1a4b0d3103d3c3
MartinReynolds90 <martinreynolds2607@gmail.com>
2025-09-24 09:19:32 -0300

    Creo el Tilemap

--- PV2ProyectUnity/Assets/Scripts/Jugador.cs
+++ PV2ProyectUnity/Assets/Scripts/Jugador.cs
@@ -6,8 +6,8 @@ using UnityEngine.Events;//agrego el .Events para poder usar eventos
 public class Jugador : MonoBehaviour
 {
     [Header("Configuracion")]
-<<<<<<< Updated upstream
-    [SerializeField] private int vida = 12;
+//<<<<<<< Updated upstream
+    [SerializeField] private int vida = 5;
     [SerializeField] private UnityEvent<int> OnLivesChanged;//declaro un evento tipo entero para avisar cuando se modifiquen las vidas
     [SerializeField] private UnityEvent<string> OnTextChanged;//lo mismo pero para cambiar el texto de las vidas
     [SerializeField] private UnityEvent OnVelocidadCero;//////////////////////
@@ -18,11 +18,11 @@ public class Jugador : MonoBehaviour
          OnLivesChanged.Invoke(vida);//inicializo los eventos con invoke
          OnTextChanged.Invoke(vida.ToString());
     }
-=======
-    [SerializeField] private float vida = 5f;
+//=======
+    //[SerializeField] private int vida = 5;
 
 
->>>>>>> Stashed changes
+//>>>>>>> Stashed changes
     // Update is called once per frame
     public void ModificarVida(int puntos)
     {
