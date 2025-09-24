7d446657b28406a99f5af0a7e5ea68237ba260db
MartinReynolds90 <martinreynolds2607@gmail.com>
2025-09-24 08:47:55 -0300

    Comit antes de tilemap

--- PV2ProyectUnity/Assets/Scripts/Jugador.cs
+++ PV2ProyectUnity/Assets/Scripts/Jugador.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;//agrego el .Events para poder usar eventos
 public class Jugador : MonoBehaviour
 {
     [Header("Configuracion")]
+<<<<<<< Updated upstream
     [SerializeField] private int vida = 12;
     [SerializeField] private UnityEvent<int> OnLivesChanged;//declaro un evento tipo entero para avisar cuando se modifiquen las vidas
     [SerializeField] private UnityEvent<string> OnTextChanged;//lo mismo pero para cambiar el texto de las vidas
@@ -17,6 +18,11 @@ public class Jugador : MonoBehaviour
          OnLivesChanged.Invoke(vida);//inicializo los eventos con invoke
          OnTextChanged.Invoke(vida.ToString());
     }
+=======
+    [SerializeField] private float vida = 5f;
+
+
+>>>>>>> Stashed changes
     // Update is called once per frame
     public void ModificarVida(int puntos)
     {
